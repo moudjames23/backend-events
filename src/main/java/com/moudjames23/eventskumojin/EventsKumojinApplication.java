41009b15fe168b80984928a1755bbe29ecc5a8a3
Mamoudou DIALLO <moud23diallo@outlook.com>
2024-03-30 22:33:42 -0400

    Initial checking

---
+++ src/main/java/com/moudjames23/eventskumojin/EventsKumojinApplication.java
@@ -0,0 +1,13 @@
+package com.moudjames23.eventskumojin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsKumojinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsKumojinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moudjames23/eventskumojin/EventsKumojinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moudjames23.eventskumojin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsKumojinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
